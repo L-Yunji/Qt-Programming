103699af37d9a672017f8928e11186f3b58a92aa
LEEYUNJI <yunji965@gmail.com>
2025-04-08 09:46:31 +0900

    🔥 Remove build files and apply .gitignore

--- DateTimeWidget/build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_DateTimeWidget_FILE C:/Users/2-08/Documents/Qt-Widget/DateTimeWidget/build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/DateTimeWidget.exe)
-set(__QT_DEPLOY_TARGET_DateTimeWidget_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_DateTimeWidget_RUNTIME_DLLS C:/Qt/6.9.0/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.9.0/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.9.0/mingw_64/bin/Qt6Core.dll)
